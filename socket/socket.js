99856577aed650463fe162519bb171b0d7723c2a
Saini <85294275+Upiottxat@users.noreply.github.com>
2024-08-13 16:17:27 +0530

    socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,33 +1,37 @@
-import server from "../server";
-const io = new Server(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: 'https://chathello.netlify.app/'
-    }
-})
-console.log("socket is on")
-
-io.on('connection', (socket) => {
-
-    console.log("connected to socket");
-    socket.on("setup", (userData) => {
-        socket.join(userData._id);
-        console.log(userData._id);
-        socket.emit("connection")
+import { Server } from "socket.io";
+const setupWebSocket = (server) => {
+    const io = new Server(server, {
+        pingTimeout: 60000,
+        cors: {
+            origin: "https://chathello.netlify.app/"
+        }
     })
+    console.log("lising socket");
 
-    socket.on("join room", (room) => {
-        socket.join(room)
-        console.log("user joined room " + room);
+
+    io.on('connection', (socket) => {
+
+        console.log("connected to socket");
+        socket.on("setup", (userData) => {
+            socket.join(userData._id);
+            console.log(userData._id);
+            socket.emit("connection")
+        })
+
+        socket.on("join room", (room) => {
+            socket.join(room)
+            console.log("user joined room " + room);
+        })
+
+        socket.on("new message", (newMessageRecieved) => {
+            if (!newMessageRecieved) return console.log("message not received");
+            // if (newMessageRecieved.senderId===)
+
+            socket.in(newMessageRecieved.senderId).emit("message received", newMessageRecieved)
+
+        })
+
+
     })
-
-    socket.on("new message", (newMessageRecieved) => {
-        if (!newMessageRecieved) return console.log("message not received");
-        // if (newMessageRecieved.senderId===)
-
-        socket.in(newMessageRecieved.senderId).emit("message received", newMessageRecieved)
-
-    })
-
-
-})
+}
+export default setupWebSocket
